949a6b74ca6b0894114ca6128372fcb1b63e9290
Faris k.p <82799675+faris-kp@users.noreply.github.com>
2024-03-19 11:21:03 +0530

    DatabaseLayerAdded

---
+++ HRLeaveManagement.Application/Persistence/Contracts/ILeaveRequestRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRLeaveManagement.Application.Persistence.Contracts
+{
+    public interface ILeaveRequestRepository:IGenericRepository<LeaveRequest>
+    {
+
+    }
+}
